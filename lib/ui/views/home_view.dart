cf5054aee98c24ce4b8ed77552a7876a635b4936
FutureGrit <app.futuregrit@gmail.com>
2020-09-18 15:38:17 +0530

    :bug: Fix import in home_view

--- lib/ui/views/home_view.dart
+++ lib/ui/views/home_view.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'file:///D:/Flutter/Projects/scale_factor/lib/ui/widgets/custom_app_bar.dart';
+import 'package:scale_factor/ui/widgets/custom_app_bar.dart';
 
 class HomeView extends StatefulWidget {
   @override
@@ -15,14 +15,14 @@ class _HomeViewState extends State<HomeView> {
         child: CustomAppBar(),
       ),
       body: Container(
-          //color: Colors.yellow,
-          // TODO 1: Create input section
-          // TODO 1.1: Implement "Platform" tabs
-          // TODO 1.2: Implement "Value" edit text
-          // TODO 1.3: Implement "Unit" and "Baseline" dropdown
+        color: Colors.yellow,
+        // TODO 1: Create input section
+        // TODO 1.1: Implement "Platform" tabs
+        // TODO 1.2: Implement "Value" edit text
+        // TODO 1.3: Implement "Unit" and "Baseline" dropdown
 
-          // TODO 2: Create output section
-          ),
+        // TODO 2: Create output section
+      ),
     );
   }
 }
